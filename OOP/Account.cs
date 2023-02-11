22e4aba5c13aed1d05023c0ea3cd79573d3d0305
Ahmed Break <a.break@ejadtech.sa>
2023-02-11 10:59:27 +0300

    Add project files.

---
+++ OOP/Account.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+    internal class Account
+    {
+        private decimal _balance;
+
+        public decimal Balance { get { return _balance; } set { _balance = value; } }
+        public Account() { 
+            Balance = 0;
+        }
+        public Account( decimal balance)
+        {
+            _balance = balance;
+        }
+
+        ~Account() { }
+
+        public event EventHandler? OnOverDraft;
+
+
+        public void Withdraw(decimal amount)
+        {
+            if (amount > _balance)
+                this.OnOverDraft(this, null);
+            else
+                _balance -= amount;
+        }
+
+        public void Deposit(decimal amount)
+        {
+            _balance += amount;
+        }
+
+        public static Account operator +(Account a, Account b)
+        {
+            return new Account { Balance = a.Balance + b.Balance };
+        }
+
+    }
+}
---
+++ OOP/JointAccount.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+    internal class JointAccount
+    {
+        private const int MaxAccountSize = 3;
+
+        private Account[] _accountList = new Account[MaxAccountSize];
+
+        public Account this[int index]
+        {
+
+            get
+            {
+                if (index >= 0 && index <= -1)
+                    return _accountList[index];
+                else
+                    return null;
+            }
+            set
+            {
+                if (index >= 0 && index <= -1)
+                    _accountList[index] = value;
+            }
+        }
+    }
+}
